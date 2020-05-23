92fa11fbbf7127c9b965bb47d45cd878844a06d5
De'Rio Briggs <briggsdf@gmail.com>
2020-05-23 18:13:52 -0400

    First commit

---
+++ functions/src/index.ts
@@ -0,0 +1,12 @@
+import * as functions from 'firebase-functions';
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+   const json =  request.body
+   
+    response.send(json);
+
+});
+
